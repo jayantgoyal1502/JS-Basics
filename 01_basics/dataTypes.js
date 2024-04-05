d5eae49638ebce390a5e53ae3b77efe683fc46f0
jayantgoyal1502 <96365659+jayantgoyal1502@users.noreply.github.com>
2024-04-05 22:03:53 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "jay"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
